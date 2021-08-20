87249bd92b2ab83dd530fd72343872e3af76c039
sceptre520 <denysshowdev@gmail.com>
2021-08-20 17:14:54 +0300

    first commit

---
+++ basic.py
@@ -0,0 +1,73 @@
+from time import sleep
+import csv
+from typing import cast
+from td.client import TDClient
+import db_conn
+
+TDSession = TDClient(
+    client_id='UGPAX5IGKEO3JRPGAEQMS4CCNH4I6GPC',
+    redirect_uri='http://127.0.0.1:3000',
+    credentials_path='token.json'
+)
+TDSession.login()
+
+def getOneStock(symbol):
+    opt_chain = {
+        'symbol': symbol,
+        'includeQuotes': True,
+    }
+    option_chains = TDSession.get_options_chain(option_chain=opt_chain)
+    stock_id = db_conn.saveStock(option_chains['underlying'])
+    # ret_symbol = option_chains['underlying']
+    # print(ret_symbol)
+    for y in option_chains['callExpDateMap']:
+        k = option_chains['callExpDateMap'][y]
+        for z in k:
+            # print(k[z][0]['symbol'])
+            db_conn.saveOption(stock_id, k[z][0])
+
+
+def catchData():
+    symbols = []
+    with open('std.csv', 'r') as file:
+        reader = csv.reader(file)
+        i=0
+        for row in reader:
+            i = i + 1
+            if i==1:
+                continue
+            symbols.append(row[0])
+    
+    try:
+        filer = open("call.log","r")
+        keyv = filer.read()
+        filer.close()
+    except:
+        keyv = ''
+
+    index = 0
+    ti = 0
+    for x in symbols:
+        if x == keyv:
+            index = ti
+        ti = ti + 1
+    print('----------')
+    print(index)
+    
+    lenv = len(symbols)
+    while(True):
+        cur_symbol = symbols[index]
+
+        print(cur_symbol)
+        getOneStock(cur_symbol)
+
+        fileh = open("call.log","w+")
+        fileh.flush()
+        fileh.write(cur_symbol)
+        fileh.close()
+        index = index + 1
+        index = index % lenv
+        sleep(1)
+
+
+catchData()
---
+++ db_conn.py
@@ -0,0 +1,141 @@
+import sqlite3
+from os.path import exists
+dbName = "build.db"
+def init():
+    if not exists(dbName):
+        connection = sqlite3.connect(dbName)
+        cursor = connection.cursor()
+        cursor.execute("""
+            CREATE TABLE "stock" (
+                "id"	INTEGER,
+                "symbol"	TEXT,
+                "description"	TEXT,
+                "change"	TEXT,
+                "percentChange"	TEXT,
+                "close"	TEXT,
+                "quoteTime"	TEXT,
+                "tradeTime"	TEXT,
+                "bid"	TEXT,
+                "ask"	TEXT,
+                "last"	TEXT,
+                "mark"	TEXT,
+                "markChange"	TEXT,
+                "markPercentChange"	TEXT,
+                "bidSize"	TEXT,
+                "askSize"	TEXT,
+                "highPrice"	TEXT,
+                "lowPrice"	TEXT,
+                "openPrice"	TEXT,
+                "totalVolume"	TEXT,
+                "exchangeName"	TEXT,
+                "fiftyTwoWeekHigh"	TEXT,
+                "fiftyTwoWeekLow"	TEXT,
+                "delayed"	TEXT,
+                PRIMARY KEY("id" AUTOINCREMENT)
+            );
+        """)
+
+
+        cursor.execute("""
+            CREATE TABLE "options" (
+                "id"	INTEGER,
+                "symbol_id"	INTEGER,
+                "putCall"	TEXT,
+                "symbol"	TEXT,
+                "description"	TEXT,
+                "exchangeName"	TEXT,
+                "bid"	TEXT,
+                "ask"	TEXT,
+                "last"	TEXT,
+                "mark"	TEXT,
+                "bidSize"	TEXT,
+                "askSize"	TEXT,
+                "bidAskSize"	TEXT,
+                "lastSize"	TEXT,
+                "highPrice"	TEXT,
+                "lowPrice"	TEXT,
+                "openPrice"	TEXT,
+                "closePrice"	TEXT,
+                "totalVolume"	TEXT,
+                "tradeDate"	TEXT,
+                "tradeTimeInLong"	TEXT,
+                "quoteTimeInLong"	TEXT,
+                "netChange"	TEXT,
+                "volatility"	TEXT,
+                "delta"	TEXT,
+                "gamma"	TEXT,
+                "theta"	TEXT,
+                "vega"	TEXT,
+                "rho"	TEXT,
+                "openInterest"	TEXT,
+                "timeValue"	TEXT,
+                "theoreticalOptionValue"	TEXT,
+                "theoreticalVolatility"	TEXT,
+                "optionDeliverablesList"	TEXT,
+                "strikePrice"	TEXT,
+                "expirationDate"	TEXT,
+                "daysToExpiration"	TEXT,
+                "expirationType"	TEXT,
+                "lastTradingDay"	TEXT,
+                "multiplier"	TEXT,
+                "settlementType"	TEXT,
+                "deliverableNote"	TEXT,
+                "isIndexOption"	TEXT,
+                "percentChange"	TEXT,
+                "markChange"	TEXT,
+                "markPercentChange"	TEXT,
+                "nonStandard"	TEXT,
+                "inTheMoney"	TEXT,
+                "mini"	TEXT,
+                PRIMARY KEY("id" AUTOINCREMENT)
+            );
+        """)
+
+        connection.commit()
+
+def saveStock(dict):
+    connection = sqlite3.connect(dbName)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM stock WHERE symbol='{data}';".format(data=dict['symbol']))
+    rows = cursor.fetchall()
+    if len(rows) == 0:
+        col = ''
+        val = ''
+        for x in dict:
+            if col != '':
+                col = col + ', '
+            if val != '':
+                val = val + ', '
+            col = col + x
+            val = val + "'" + str(dict[x]) + "'"
+        cursor.execute("INSERT INTO stock({col}) VALUES ({val});".format(col=col,val=val))
+        ret = cursor.lastrowid
+    else:
+        set_str = ''
+        for x in dict:
+            if set_str != '':
+                set_str = set_str + ', '
+            set_str = set_str + x + "='" + str(dict[x]) + "'"
+        cursor.execute("UPDATE stock SET {set_str} WHERE id={id};".format(set_str=set_str,id=rows[0][0]))
+        ret = rows[0][0]
+    connection.commit()
+    return ret
+
+def saveOption(stock_id, dict):
+    connection = sqlite3.connect(dbName)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM options WHERE symbol='{data}' AND symbol_id={symbol_id};".format(data=dict['symbol'], symbol_id=stock_id))
+    rows = cursor.fetchall()
+    if len(rows) == 0:
+        col = 'symbol_id'
+        val = str(stock_id)
+        for x in dict:
+            col = col + ', ' + x
+            val = val + ', ' + "'" + str(dict[x]) + "'"
+        cursor.execute("INSERT INTO options({col}) VALUES ({val});".format(col=col,val=val))
+    else:
+        set_str = 'symbol_id='+str(stock_id)
+        for x in dict:
+            set_str = set_str + ', '  + x + "='" + str(dict[x]) + "'"
+        cursor.execute("UPDATE options SET {set_str} WHERE id={id};".format(set_str=set_str,id=rows[0][0]))
+    connection.commit()
